86b3a32005aecfa6dc3eb92885e523025b450cf9
= <muhbinrang650@gmail.com>
2022-12-23 10:09:42 +0800

    Feat Cards

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Navbar from '@/Components/Navbar'
-import { AiOutlineStock } from 'react-icons/ai';
-import { FaCity } from 'react-icons/fa';
-import Icon from '@/Components/Icon'
 import Menus from '@/Components/Menus';
+import Card from '@/Components/Card';
 
 const Home = () => {
     const filteringMenus = [
@@ -102,36 +100,9 @@ const Home = () => {
             <Navbar />
             <div className="bg-white rounded-tl-md rounded-tr-md">
                 <Menus filteringMenus={filteringMenus} />
-                <div className="">
+                <div>
                     <h2 className='px-3 py-2 text-lg font-semibold'>Produk Hari Ini</h2>
-                    <div className="py-3 overflow-x-auto flex flex-row gap-x-2 scrollbar-hide bg-green-700">
-                        {products.map((product, i) => (
-                            <div key={i} className="flex flex-row justify-evenly">
-                                <div className="overflow-hidden w-[150px] h-[250px] rounded-lg bg-white">
-                                    <div className="overflow-hidden w-full h-1/2">
-                                        <img src="https://source.unsplash.com/1600x900/?clothes" className='w-full h-full object-cover' alt="" />
-                                    </div>
-                                    <div className="flex flex-col py-1 px-2 gap-y-1">
-                                        <h2 className='text-lg font-bold leading-3 mt-2'>Rp 100.000</h2>
-                                        <div className='mt-1'>
-                                            <div className='mb-1'>
-                                                <div className='bg-red-600 text-white text-xs rounded-sm px-1 inline-block mr-1'>10%</div>
-                                                <span className='text-xs inline-block line-through text-slate-400'>Rp 150.000</span>
-                                            </div>
-                                            <div className='mb-1'>
-                                                <FaCity className='inline-block mr-1 text-violet-600' />
-                                                <span className='text-xs text-slate-400'>Kota Makassar</span>
-                                            </div>
-                                            <div>
-                                                <AiOutlineStock className='inline-block mr-1 text-yellow-600' />
-                                                <span className='text-xs text-slate-400'>250 Stock</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
+                    <Card products={products} />
                 </div>
             </div>
         </div>
